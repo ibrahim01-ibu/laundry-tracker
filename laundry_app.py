3a6b8637d6f0f2ede7ee889149c8d86e1ccabb69
ibrahim01-ibu <seeker0to1@gmail.com>
2025-07-08 22:09:37 +0530

    laundry_app.py

--- laundry_app.py
+++ laundry_app.py
@@ -14,6 +14,7 @@ import json
 from google.oauth2.service_account import Credentials
 
 # ------------------ SETUP GOOGLE SHEETS + DRIVE ------------------ #
+scope = ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive']
 creds_dict = st.secrets["gcp_service_account"]
 creds = Credentials.from_service_account_info(creds_dict, scopes=scope)
 # Google Sheets & Drive auth
